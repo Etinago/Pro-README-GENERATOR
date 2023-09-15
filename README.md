5bdd2370e01deff5cb1452c9ff13794d0dec6e8e
Etinago <124711424+Etinago@users.noreply.github.com>
2023-09-15 19:19:45 +0100

    updated script

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 
-# Automatic ReadMe generator
+# Automatic Readme Generator 
 
-Q
+This project allows the user to generate a READme File using node js within term by prompting user to answer a few questions.
 
 ## Installation
-Q
+Inorder to use this application download the repository and make sure the index.js in the root path of your folder. To run just type node js. NB: You should have a version of node downloaded 
 
 ## Usage
-Q
+q
 
 ## Contributing
-Q
+q
 
 ## License
-This project is licensed under the Q License.
+This project is licensed under the q License.
 
 ---
 
--- index.js
+++ index.js
@@ -6,52 +6,38 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Intialise an array to store the section headers 
-
-const tableContents = [];
-
 rl.question('Enter the title of your project: ', (title) => {
-  sections.push(`# ${title}\n\n`);
-
   rl.question('Enter a description of your project: ', (description) => {
-    sections.push(`## Description\n${description}\n\n`);
-
     rl.question('Enter installation instructions: ', (installation) => {
-      sections.push(`## Installation\n${installation}\n\n`);
-
       rl.question('Enter usage information: ', (usage) => {
-        sections.push(`## Usage\n${usage}\n\n`);
-
         rl.question('Enter contribution guidelines: ', (contributing) => {
-          sections.push(`## Contributing\n${contributing}\n\n`);
+          rl.question('Enter the project license: ', (license) => {
+            const readmeContent = `
+# ${title}
 
-          r1.question('Enter the testing information requirements: ', (tests) => {
-            section.push(`## Tests\n${Tests}\n\n`);
+${description}
 
-            rl.question('Enter the project license: ', (license) => {
-              sections.push(`## License\nThis project is licensed under the ${license} License.\n\n`);
+## Installation
+${installation}
 
-            // Creating the table of contents using map function
-            const tableOfContents = sections.map((section) => {
-              
-              
-              
-              // Extracting section header
-              const sectionHeader = section.match(/## (.+?)\n/);
-              if (sectionHeader) {
-                return `- [${sectionHeader[1]}](#${sectionHeader[1].toLowerCase().replace(/ /g, '-')})`;
-              }
-              return '';
-            }).join('\n');
+## Usage
+${usage}
 
-            // Combining the sections and table of contents
-            const readmeContent = `${tableOfContents}\n\n${sections.join('')}`;
+## Contributing
+${contributing}
+
+## License
+This project is licensed under the ${license} License.
+
+---
+
+**Note:** This is a generated README.md file.
+`;
 
             fs.writeFile('README.md', readmeContent, (err) => {
               if (err) throw err;
               console.log('README.md file has been created successfully.');
               rl.close();
-            });
             });
           });
         });
